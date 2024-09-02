750f6bdd1c33ec5569473c71dfb690c9b9877df4
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-09-02 14:24:38 +0100

    webkit

--- src/App.vue
+++ src/App.vue
@@ -3,77 +3,63 @@ import { RouterView } from 'vue-router'
 import AppNav from './components/App/AppNav/AppNav.vue'
 import AppFooter from './components/App/AppFooter/AppFooter.vue'
 
-
-
-
-
-
-
-
 </script>
 
 <template>
-
    <header>
       <h1>a vue to a build</h1>
-      <!-- class="app_nav"  - distinct from main_nav (toc) below -->
    </header>
-
    <main>
-
       <AppNav />
-
-      <section class="main_content">
-         <RouterView />
-      </section>
-
+      <RouterView />
    </main>
-
    <AppFooter />
-
 </template>
 
-<style scoped>
 
+<style scoped>
 main {
    position:relative;
+   
+   display:-ms-grid;
    display:grid;
-   grid-template-columns: 1fr;
+
+   -ms-grid-columns:1fr;
+   grid-template-columns:1fr;
+
    min-height:120vh;
 }
 @media (min-width: 1024px) {
    main {
-      grid-template-columns: 1fr 3.5fr;
+      -ms-grid-columns:1fr 3.5fr;
+      grid-template-columns:1fr 3.5fr;
    }
 }
-
 h1 {
    font-weight:300;
    font-size:2.5rem;
    font-style:italic;
    font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
 }
-
-.logo {
-   display: block;
-   margin: 0 auto 2rem;
-}
-
-
-
 header {
    padding:1rem 2rem 1rem 2rem;
    background:hsl(0, 0%, 95%);
 }
 @media (min-width: 1024px) {
    header {
+      display:-webkit-box;
+      display:-ms-flexbox;
       display: flex;
-      place-items: center;
-      padding-right: calc(var(--section-gap) / 2);
-   }
 
-   .logo {
-      margin: 0 2rem 0 0;
+      -webkit-box-pack:center;
+      -ms-flex-pack:center;
+      justify-content:center;
+
+      -webkit-box-align:center;
+      -ms-flex-align:center;
+      align-items:center;
+
+      /* padding-right: calc(var(--section-gap) / 2); */
    }
 
 }
