6c0f5d5cc735ebc2efbb7188c5760b2f5f864f2c
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-09-09 11:17:46 +0100

    add window.scroll

--- src/components/NotFound/NotFound.vue
+++ src/components/NotFound/NotFound.vue
@@ -1,19 +1,17 @@
 <script setup lang="ts">
 
+window.scroll(0,0)
+
 </script>
 
-
 <template>
-
    <section class="aspect_view">
       <h1>page not found</h1>
    </section>
-
 </template>
 
 
 <style scoped>
-
 section.aspect_view {
    margin-top:2rem;
 }
