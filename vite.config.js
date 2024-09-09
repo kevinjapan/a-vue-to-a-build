5f11099fc0fe9fdb242f30e07fe494b139b12efe
kevinjapan <61291412+kevinjapan@users.noreply.github.com>
2024-09-09 11:16:29 +0100

    build for our older IOS test suite

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,11 @@ export default defineConfig({
       alias: {
          '@': fileURLToPath(new URL('./src', import.meta.url))
       }
+   },
+   // we configure build to meet older IOS version 12.
+   // IOS v12 was simply displaying a blank screen, by default, vite builds for ES Modules (so es2022)
+   // https://vitejs.dev/guide/build.html#browser-compatibility https://vitejs.dev/config/build-options.html#build-target
+   build: {
+     target: "es2015"
    }
 })
